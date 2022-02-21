acdf0addc814ca160fa1d28de0181ff583cebeae
Magic-Stones <davesolayao123@gmail.com>
2022-02-21 15:53:35 +0800

    Update MenuStateScript.cs

--- First Brackeys Game Jam/Assets/Scripts/MenuStateScript.cs
+++ First Brackeys Game Jam/Assets/Scripts/MenuStateScript.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MenuStateScript : MonoBehaviour
 {
-
     #region Start & Update functions
     // Start is called before the first frame update
     void Start()
